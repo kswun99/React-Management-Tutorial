5bdef59bc08b7aae55780a12b41da74972662df3
Justin <kswun99@hanmail.net>
2024-01-16 19:54:49 +0900

    배열 Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,46 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Customer from "./components/Customer";
 
-const customer = {
-  name: "홍길동",
-  birthday: "961222",
-  gender: "남자",
-  job: "대학생",
-};
+const customers = [
+  {
+    id: 0,
+    image: "https://picsum.photos/200/300",
+    name: "심청이",
+    birthday: "710910",
+    gender: "여자",
+    job: "고등학생",
+  },
+  {
+    id: 1,
+    image: "https://picsum.photos/200/300",
+    name: "홍길동",
+    birthday: "961222",
+    gender: "남자",
+    job: "대학생",
+  },
+];
 
 function App() {
   return (
-    <Customer
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    />
+    <div>
+      <Customer
+        id={customers[0].id}
+        image={customers[0].image}
+        name={customers[0].name}
+        birthday={customers[0].birthday}
+        gender={customers[0].gender}
+        job={customers[0].job}
+      />
+      <Customer
+        id={customers[1].id}
+        image={customers[1].image}
+        name={customers[1].name}
+        birthday={customers[1].birthday}
+        gender={customers[1].gender}
+        job={customers[1].job}
+      />
+    </div>
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
--- src/components/Customer.tsx
+++ src/components/Customer.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 
 interface CustomerProps {
+  id: number;
+  image: string;
   name: string;
   birthday: string;
   gender: string;
   job: string;
 }
 
-const Customer: React.FC<CustomerProps> = ({ name, birthday, gender, job }) => {
+const Customer: React.FC<CustomerProps> = ({
+  id,
+  image,
+  name,
+  birthday,
+  gender,
+  job,
+}) => {
   return (
     <div>
+      <h2>{id}</h2>
+      <img src={image} alt="profile" />
       <h2>Customer Name: {name}</h2>
       <p>Birthday: {birthday}</p>
       <p>Gender: {gender}</p>
