3ffeeb2e88095209b95d50375eb038502b69e6ee
Justin <kswun99@hanmail.net>
2024-01-16 21:23:06 +0900

    테마 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
 import React from "react";
 import "./App.css";
 import Customer from "./components/Customer";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import { Theme } from "@mui/material/styles";
+
+const styles = (theme: Theme) => ({
+  root: {
+    width: "100%",
+    overflowX: "auto",
+  },
+  table: {
+    minWidth: 1080,
+  },
+});
 
 const customers = [
   {
@@ -21,23 +38,39 @@ const customers = [
   },
 ];
 
-function App() {
+const App: React.FC = () => {
+  const customStyles = styles({} as Theme);
+
   return (
-    <div>
-      {customers.map((c) => {
-        return (
-          <Customer
-            key={c.id}
-            id={c.id}
-            image={c.image}
-            name={c.name}
-            birthday={c.birthday}
-            gender={c.gender}
-            job={c.job}
-          />
-        );
-      })}
-    </div>
+    <Paper sx={{ ...customStyles.root, mt: 3 }}>
+      <Table sx={{ ...customStyles.table }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map((c) => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
@@ -55,6 +88,6 @@ function App() {
     //   </header>
     // </div>
   );
-}
+};
 
 export default App;
